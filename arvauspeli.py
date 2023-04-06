dbb895f2ff9d41cd2877c667e21b4acabde29f62
teeomasi <112539960+teeomasi@users.noreply.github.com>
2023-04-06 21:01:00 +0300

    arvauspeli toimii

---
+++ arvauspeli.py
@@ -0,0 +1,5 @@
+viikonpäivä =  input ("mikä viikonpäivä nyt on?:")
+if viikonpäivä== "lauantai" or viikonpäivä== "sunnuntai":
+    print ("nyt on vklp :3")
+else: 
+    print ("nyt on arkipäivä :(")
