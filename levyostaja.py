368291893b6ee5be0b3396985c530994ee15da4b
teeomasi <112539960+teeomasi@users.noreply.github.com>
2023-04-12 17:22:52 +0300

    123

---
+++ levyostaja.py
@@ -0,0 +1,8 @@
+rahamäärä = int(input("paljos sulla on massia:"))
+levy = 20
+while rahamäärä < 20:
+  rahamäärä - levy
+else:
+  print ("ei riitä ennää rahet")
+  print ("sait levyjä:")
+  print (f"{rahamäärä/levy} ")
